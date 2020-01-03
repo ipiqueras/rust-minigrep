22f5c7b03ccdedb83742a7db4da1805ceccc4ab0
piqueras <ignacio.piqueras@silicon-gears.com>
2020-01-03 15:02:30 +0100

    Initial import, as in the official documentation

---
+++ README.md
@@ -0,0 +1,7 @@
+# Learning Rust: minigrep
+
+Implement [minigrep](https://doc.rust-lang.org/book/ch12-00-an-io-project.html) 
+and slowly add some functionality: logging, better argument passing, integration
+tests and documentation.
+
+This is just a small private project to learn Rust.
---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use std::env;
+use std::process;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = minigrep::Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+    println!("search: {}", config.query);
+    println!("in: {}", config.filename);
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
+}
